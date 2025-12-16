e3b37ec0c485c21f02e8c463c9621bd6435d9d9c
kdezer <kdezer1992@gmail.com>
2025-12-16 21:43:46 +0100

    add pydantic settings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from core.config.config_loader import Config
+from core.config.settings import Settings
 from core.domains.location import Location
 from infrastructure.messengers.telegram_messenger import TelegramMessenger
 from infrastructure.weather_api_client import WeatherApiClient
@@ -8,12 +8,13 @@ from core.logging_setup import setup_logging
 
 
 def run():
+    settings = Settings()
+
     setup_logging()
-    config = Config.from_files()
-    location = Location(name=config.name, latitude=config.latitude, longitude=config.longitude)
+    location = Location(name=settings.location_name, latitude=settings.latitude, longitude=settings.longitude)
     weather_client = WeatherApiClient()
     weather_service = WeatherService(weather_client)
-    messenger = TelegramMessenger(chat_id=config.telegram_chat_id, api_token=config.telegram_api_token)
+    messenger = TelegramMessenger(chat_id=settings.telegram_chat_id, api_token=settings.telegram_api_token)
     notification_service = WeatherNotificationService(messenger=messenger, weather_service=weather_service)
     notification_service.run(location=location)
 
